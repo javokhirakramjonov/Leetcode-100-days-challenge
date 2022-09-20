143929214bd8be7eb9c07f6c7f3f26f38ae05182
javokhirakramjonov <88347670+javokhirakramjonov@users.noreply.github.com>
2022-09-21 00:32:41 +0500

    Create day_16_find_smallest_letter_greater_than_target.cpp

---
+++ day_16_find_smallest_letter_greater_than_target.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = 0, r = letters.size() - 1, m;
+        while(l < r) {
+            m = l + (r - l) / 2;
+            if(letters[m] <= target)
+                l = m + 1;
+            else
+                r = m;
+        }
+        if(letters[l] <= target)
+            l = 0;
+        return letters[l];
+    }
+};
